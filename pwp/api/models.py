e66d80936f0ec5e4849bf7e0bdac50e32f36ec54
Erkka Makinen <erkka.makinen@gmail.com>
2016-05-28 14:12:15 +0300

    More models

--- pwp/api/models.py
+++ pwp/api/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 # Create your models here.
 class Rule(models.Model):
-  rule_text = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  modified_at = models.DateTimeField(auto_now=True)
+    rule_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    rule = models.ForeignKey('Rule', on_delete=models.CASCADE)
+
+class User(models.Model):
+    nickname = models.CharField(max_length=30)
+    realname = models.CharField(max_length=50)
