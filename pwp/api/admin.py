20fb8fb5a6c9b23a483ac98ac3fbc5ead44e619c
Erkka Makinen <erkka.makinen@gmail.com>
2016-05-28 14:14:57 +0300

    Rule -> Article

--- pwp/api/admin.py
+++ pwp/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Rule
+from .models import Article, Comment, User
 
-admin.site.register(Rule)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(User)
--- pwp/api/models.py
+++ pwp/api/models.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Rule(models.Model):
-    rule_text = models.TextField()
+class Article(models.Model):
+    topic = models.TextField()
+    article_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
@@ -12,7 +13,7 @@ class Comment(models.Model):
     comment_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    rule = models.ForeignKey('Rule', on_delete=models.CASCADE)
+    rule = models.ForeignKey('Article', on_delete=models.CASCADE)
 
 class User(models.Model):
     nickname = models.CharField(max_length=30)
